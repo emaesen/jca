10e259f32f392f3d3b6093c8b9065c92de6b6fb3
Edward <edward.maesen@gmail.com>
2019-08-13 10:53:52 -0700

    Add config scaffold

---
+++ client/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      // pass options to less-loader
+      // http://lesscss.org/usage/#less-options
+      less:{
+        
+      }
+    }
+  }
+}
