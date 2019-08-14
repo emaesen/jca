c79adc70df3cacb6a27e738d00de9d9cf14f3538
Edward <edward.maesen@gmail.com>
2019-08-14 13:22:49 -0700

    Add routes

--- client/src/router.js
+++ client/src/router.js
@@ -5,6 +5,12 @@ import Home from './views/Home.vue';
 import GetInvolved from './views/GetInvolved.vue';
 import AboutMore from './views/AboutMore.vue';
 import Contact from './views/Contact.vue';
+import Events from './views/Events.vue';
+import Music from './views/Music.vue';
+import Theater from './views/Theater.vue';
+import VisualArts from './views/VisualArts.vue';
+import VenueInfoBooking from './views/VenueInfoBooking.vue';
+import About from './views/About.vue';
 import OverlayFacebook from './views/OverlayFacebook.vue';
 
 Vue.use(Router)
@@ -30,6 +36,36 @@ export default new Router({
       meta: { 
         transitionName: 'slide'
       },
+    },
+    {
+      path: '/events',
+      name: 'events',
+      component: Events,
+    },
+    {
+      path: '/music',
+      name: 'music',
+      component: Music,
+    },
+    {
+      path: '/theater',
+      name: 'theater',
+      component: Theater,
+    },
+    {
+      path: '/visual-arts',
+      name: 'visual-arts',
+      component: VisualArts,
+    },
+    {
+      path: '/venue-info-booking',
+      name: 'venue-info-booking',
+      component: VenueInfoBooking,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
     },
     {
       path: '/contact',
