a3aab6c78b17be5a908e69cc660ad7a6f8dae620
Edward <edward.maesen@gmail.com>
2019-08-14 14:39:06 -0700

    Add placeholder file

---
+++ client/src/views/AboutMore.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-more">
+    <h1></h1>
+    <p>
+      
+    </p>
+
+    <h2></h2>
+    <p>
+      
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutMore'
+};
+</script>
