ea293aa83576efd055a8c93f64aec8d25122ff11
Edward <edward.maesen@gmail.com>
2019-08-15 11:36:20 -0700

    Switch to icon components

--- client/src/components/SiteNav.vue
+++ client/src/components/SiteNav.vue
@@ -13,9 +13,7 @@
     >
       <router-link to="/events">
         Events
-        <svg version="1.1" role="presentation" width="9" height="14.4" viewBox="0 0 320 512" :class="['svg-icon icon_transition', {flip:isNavEventsExpanded}]">
-          <path d="M31.3 192H288.6C306.4 192 315.3 213.5 302.7 226.1L174.1 354.8C166.3 362.6 153.6 362.6 145.8 354.8L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
-        </svg>
+        <icon-arrow-down :class="['svg-icon icon_transition', {flip:isNavEventsExpanded}]"/>
       </router-link>
       <ul :class="['nav submenu', {collapsed:!isNavEventsExpanded}]" role="menu">
         <li class="nav item" role="menuitem">
@@ -45,8 +43,13 @@
 </template>
 
 <script>
+import IconArrowDown from '@/components/IconArrowDown.vue';
+
 export default {
   name: 'SiteNav',
+  components: {
+    IconArrowDown
+  },
   data() {
     return {
       isNavEventsExpanded: false,
