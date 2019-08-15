f368c8be4dcd3250fd53c089bca0e63f3c4b9b73
Edward <edward.maesen@gmail.com>
2019-08-14 17:22:56 -0700

    Tweak menu collapse functionality

--- client/src/components/SiteNav.vue
+++ client/src/components/SiteNav.vue
@@ -16,7 +16,7 @@
           <path d="M31.3 192H288.6C306.4 192 315.3 213.5 302.7 226.1L174.1 354.8C166.3 362.6 153.6 362.6 145.8 354.8L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
         </svg>
       </router-link>
-      <ul v-if="isNavEventsExpanded" class="nav submenu" role="menu">
+      <ul :class="['nav submenu', {collapsed:!isNavEventsExpanded}]" role="menu">
         <li class="nav item" role="menuitem">
           <router-link to="/music">Music</router-link>
         </li>
@@ -94,5 +94,8 @@ ul.nav {
 }
 .nav.submenu .nav.item {
   float: none;
+}
+.collapsed {
+  visibility: hidden;
 }
 </style>
