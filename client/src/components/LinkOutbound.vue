1734be2b430591f9690a14d84b780b1df77be0a8
Edward <edward.maesen@gmail.com>
2019-08-17 15:00:36 -0700

    Add outbound link

---
+++ client/src/components/LinkOutbound.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="to" target="_blank" rel="noopener noreferrer"><slot></slot><icon-outbound class="deemph"/></a>
+</template>
+
+<script>
+import IconOutbound from '@/components/IconOutbound.vue';
+
+export default {
+  name: 'LinkOutbound',
+  props: {
+    to: {
+      type: String
+    }
+  },
+  components: {
+    IconOutbound
+  }
+}
+</script>
+
+<style lang="less">
+  .icon.deemph {
+    color: #cccccccc;
+    width: 1em;
+  }
+</style>
