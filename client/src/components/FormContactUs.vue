052caf2811c2a59e2f14cc90bf41de1c6bdcf4cb
Edward <edward.maesen@gmail.com>
2019-08-20 14:03:33 -0700

    Move contact form to component

---
+++ client/src/components/FormContactUs.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+
+    <h2>We would love to hear from you!</h2>
+
+    <div v-if="isSubmitDone">
+      Thank you!! Your email has been sent and we look forward to contacting you soon!
+    </div>
+
+    <div v-if="!isSubmitDone">
+      <form method="post" action="mailto:info@jeffersoncenterforthearts.com">
+        <p data-id="name" data-type="input">
+          <label for="contact_name">Name</label>
+          <input type="text" id="contact_name" value="" name="contact_name" data-required_mark="required" data-field_type="input" data-original_id="name" placeholder="Name">
+        </p>
+        
+        <p data-id="email" data-type="email">
+          <label for="contact_email">Email Address</label>
+          <input type="text" id="contact_email" value="" name="contact_email" data-required_mark="required" data-field_type="email" data-original_id="email" placeholder="Email Address">
+        </p>
+        
+        <p data-id="message" data-type="text">
+          <label for="contact_message">Message</label>
+          <textarea name="contact_message" id="contact_message" data-required_mark="required" data-field_type="text" data-original_id="message" placeholder="Message"></textarea>
+        </p>
+
+        <input type="hidden" value="contact_proccess" name="contactform_submit">
+
+        <div>
+          <button type="submit">SEND</button>
+        </div>
+      </form>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormContactUs',
+  components: {
+  },
+  data() {
+    return {
+      isSubmitDone: false
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+label, input, textarea {
+  display: block;
+}
+</style>
