2412551b5b5d9a2232855b04951613de8a0bee5f
Edward <edward.maesen@gmail.com>
2019-08-23 16:58:45 -0700

    Add logo animation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="/img/jca-logo-sm.png" alt="JCA logo" class="logo_sm"/>
+    <img src="/img/jca-logo-sm.png" alt="JCA logo" class="logo_sm anima__flicker"/>
 
     <site-nav/>
 
