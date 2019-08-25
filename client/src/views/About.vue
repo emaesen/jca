cb8869d1309f89009b8dc5b48ad0fe3235b0743e
Edward <edward.maesen@gmail.com>
2019-08-25 16:10:38 -0700

    Add About paragraph

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,9 +2,10 @@
   <div>
     <h1>Jefferson Center for the Arts (JCA)</h1>
 
-    <h2>Mount Shasta's Premiere Educational Center for Music and the Performing Arts!</h2>
+    <h3>Mount Shasta's Premier Educational Center for Music and the Performing Arts!</h3>
 
     <p>
+      Jefferson Center for the Arts (JCA) is Mount Shasta’s premier arts education venue incorporating the natural beauty of the area for the purpose of improving social, economic and cultural assets. The JCA is a regional hub for Live Music, Theatre and Visual Arts for local residents and tourists alike.
     </p>
 
   </div>
