e4ffe3c07598e3e7c4e23ca28e3bf55e4c4a4e5a
Edward <edward.maesen@gmail.com>
2019-08-24 22:37:04 -0700

    Make debounce method private

--- client/src/components/mixins/window_size.js
+++ client/src/components/mixins/window_size.js
@@ -1,6 +1,30 @@
 /**
  * mixin to watch window resize and add/update window size properties
  */
+
+const debounce = (func,wait,immediate) => {
+  //Borrowed from underscore.js.
+  // For frequently firing listener functions like scroll or resize.
+  // Returns a function, that, as long as it continues to be invoked, 
+  // will not be triggered. The function `func` will be called after
+  // this stops being invoked for `wait` milliseconds. 
+  // If `immediate` is passed, trigger the function `func` on the
+  // leading edge, instead of the trailing.
+  var timeout;
+  return function() {
+    var context = this;
+    var args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
 export default {
   data () {
     return {
@@ -51,33 +75,11 @@ export default {
       this.windowRatio = h/w;
       this.windowMaxDim = m;
     },
-    debounce(func,wait,immediate) {
-      //Borrowed from underscore.js.
-      // For frequently firing listener functions like scroll or resize.
-      // Returns a function, that, as long as it continues to be invoked, 
-      // will not be triggered. The function `func` will be called after
-      // this stops being invoked for `wait` milliseconds. 
-      // If `immediate` is passed, trigger the function `func` on the
-      // leading edge, instead of the trailing.
-      var timeout;
-      return function() {
-        var context = this;
-        var args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-      };
-    },
     addResizeListener(cb) {
       if (window.addEventListener) {
         window.addEventListener(
           'resize', 
-          this.debounce(cb, 200),
+          debounce(cb, 200),
           false
         );
       }
