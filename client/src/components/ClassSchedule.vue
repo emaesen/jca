2448c5cf594d12d4d0b6a8c8f42a888b9167b46d
Edward <edward.maesen@gmail.com>
2019-08-24 17:19:07 -0700

    Add placeholder component for ClassSchedule

---
+++ client/src/components/ClassSchedule.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClassSchedule',
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import '../assets/variab.less';
+
+</style>
