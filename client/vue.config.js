5c6a4836dfcc4d61d6271023b69d0e245c72dbd2
Edward <edward.maesen@gmail.com>
2019-08-26 11:40:23 -0700

    Set PWA config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      // pass options to less-loader
-      // http://lesscss.org/usage/#less-options
-      less:{
-        
-      }
+  //https://cli.vuejs.org/config/
+  productionSourceMap: false,
+  //https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwaage/@vue/cli-plugin-pwa/v/3.0.0-rc.1
+  pwa: {
+    name: "Jefferson Center for the Arts",
+    themeColor: "#ffffff",
+    msTileColor: "#000000",
+    iconPaths: {
+      favicon32: "img/icon-32x32.png",
+      favicon16: "img/icon-16x16.png",
+      appleTouchIcon: "img/icon-152x152.png",
+      maskIcon: "img/icon.svg",
+      msTileImage: "img/icon-144x144.png"
+    },
+    //https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
+    workboxOptions: {
+      navigateFallback: "/index.html",
+      exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/,/img\/media/,/img\/icons/,/img\/video/,/^google/,/^robots/,/^sitemap/]
     }
   }
-}
+};
