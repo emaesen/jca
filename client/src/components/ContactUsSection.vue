c3a6945803d0d0844aac761714ec64c93136e32d
Edward <edward.maesen@gmail.com>
2019-08-28 12:30:36 -0700

    Implement mailto contact functionality

--- client/src/components/ContactUsSection.vue
+++ client/src/components/ContactUsSection.vue
@@ -3,33 +3,37 @@
 
     <h2>We would love to hear from you!</h2>
 
-    <div v-if="isSubmitDone">
-      Thank you!! Your email has been sent and we look forward to contacting you soon!
-    </div>
-
-    <div v-if="!isSubmitDone">
-      <form method="post" action="mailto:info@jeffersoncenterforthearts.com">
-        <p data-id="name" data-type="input">
-          <label for="contact_name">Name</label>
-          <input type="text" id="contact_name" value="" name="contact_name" data-required_mark="required" data-field_type="input" data-original_id="name" placeholder="Name">
+    <div>
+      <form method="post" :action="mailAction">
+        <p>
+          <label for="name">Your Name</label>
+          <input 
+            type="text" 
+            id="name" 
+            name="name"
+            v-model="name" 
+            placeholder="Name">
         </p>
-        
-        <p data-id="email" data-type="email">
-          <label for="contact_email">Email Address</label>
-          <input type="text" id="contact_email" value="" name="contact_email" data-required_mark="required" data-field_type="email" data-original_id="email" placeholder="Email Address">
+        <p>
+          <label for="message">Your Message</label>
+          <textarea
+            id="message"
+            name="message"
+            v-model="message"
+            placeholder="Message"></textarea>
         </p>
-        
-        <p data-id="message" data-type="text">
-          <label for="contact_message">Message</label>
-          <textarea name="contact_message" id="contact_message" data-required_mark="required" data-field_type="text" data-original_id="message" placeholder="Message"></textarea>
-        </p>
-
-        <input type="hidden" value="contact_proccess" name="contactform_submit">
 
         <div>
-          <button type="submit" class="action">SEND</button>
+          <button type="submit" :class="['action', {disabled:disableSubmit}]" :disabled=disableSubmit @click="submitForm">SEND</button><span v-if="!disableSubmit" class="deemph clarify">via your email program</span>
         </div>
       </form>
+
+      <div v-if="isSubmitDone" class="thanks">
+        Thank you!! Your email program should open, allowing you to email your message from your current address.
+        <div class="deemph">(Re-directing to your email program ensures we get a valid email address to reply to, you get to keep a copy of your send message, and it adds a hurdle for those pesky spam bots)</div>
+        <div class="deemph">If your email program did not open, you can contact JCA directly at <a :href="'mailto:' + emailTo">{{ emailTo }}</a></div>
+      </div>
+
     </div>
 
   </div>
@@ -42,13 +46,59 @@ export default {
   },
   data() {
     return {
-      isSubmitDone: false
+      isSubmitDone: false,
+      emailTo: "info@jeffersoncenterforthearts.com",
+      name: null,
+      message: null
     }
   },
+  computed: {
+    disableSubmit() {
+      return !(this.name && this.message) || this.isSubmitDone;
+    },
+    emailSubject() {
+      return "contact request from " + this.name;
+    },
+    mailAction() {
+      //console.log({subject:this.emailSubject, body:this.message})
+      return "mailto:" + this.emailTo 
+        + "?subject=" + encodeURIComponent(this.emailSubject) 
+        + "&body=" + encodeURIComponent(this.message);
+    },
+  },
+  methods: {
+    submitForm() {
+      setTimeout(() => {
+        this.isSubmitDone = true;
+      }, 100);
+    }
+  },
+  watch: {
+    name() {
+      this.isSubmitDone = false;
+    },
+    message() {
+      this.isSubmitDone = false;
+    },
+  }
 };
 </script>
 
 <style lang="less" scoped>
+@import '../assets/anima.less';
+
+.action:not(.disabled) {
+  .anima__flicker();
+}
+.clarify {
+  display: inline-block;
+  .anima__fade-in-bounce();
+  animation-delay: 2s;
+}
+.thanks {
+  .anima__zoom();
+}
+
 label, input, textarea {
   display: block;
 }
