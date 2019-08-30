027386af73388984bbc48301d312027c7ad98e75
Edward <edward.maesen@gmail.com>
2019-08-30 16:09:59 -0700

    Adding events data WIP

--- client/src/components/EventCalendar.vue
+++ client/src/components/EventCalendar.vue
@@ -19,9 +19,11 @@ import Calendar from "./Calendar";
 import eventsJson from '@/data/events.json';
 import classesJson from '@/data/classes.json';
 
+import date from './mixins/date.js'
 
 export default {
   name: "EventCalendar",
+  mixins: [date],
   components: {
     Calendar,
   },
@@ -31,29 +33,46 @@ export default {
     return { 
       nrWeeksToShow: 4,
       eventCategories: [],
+      eventTypes: [],
     };
   },
   mounted() {
+    this.setEventCategories();
+    this.setEventTypes();
   },
   created() {
     
   },
   computed: {
-
     events() {
-//      return this.user ? this.findEventsInStore({ query: {} }).data : [];
-      return [];
+      return eventsJson.events
+        .filter(e => !this.isPastDate(e.date.end ? e.date.end : e.date.start) )
+        .map(e => {if(!e.date.end){e.date.end=e.date.start} return e;});
     },
+    weeklyEvents() {
+      return classesJson.classes.weekly;
+    },
+    noWeeklyEventDates() {
+      return classesJson.classes.noClassesDates;
+    }
   },
   methods: {
     setEventCategories() {
-      // get list of user-defined eventCategories and remove duplicates
+      // get list of eventCategories and remove duplicates
       this.eventCategories = this.events
         .map(e => e.category)
         .filter((c, i, s) => s.indexOf(c) === i)
         .sort();
-      //console.log({ eventCategories: this.eventCategories });
-    }
+      console.log({ eventCategories: this.eventCategories });
+    },
+    setEventTypes() {
+      // get list of eventTypes and remove duplicates
+      this.eventTypes = this.events
+        .map(e => e.type)
+        .filter((c, i, s) => s.indexOf(c) === i)
+        .sort();
+      console.log({ eventTypes: this.eventTypes });
+    },
   }
 };
 </script>
