c1d48dad8aaa656dcde54b884e4c995a1b10fb3a
Edward <edward.maesen@gmail.com>
2019-08-29 17:18:40 -0700

    Tweak content

--- client/src/components/ClassSchedule.vue
+++ client/src/components/ClassSchedule.vue
@@ -1,13 +1,25 @@
 <template>
-  <calendar :nrWeeksToShow="nrWeeksToShow"/>
+  <div>
+    <h3>JCA's weekly class schedule:</h3>
+
+    <week-schedule :classes="classes.weekly"/>
+
+    <h3>Classes at the JCA for the coming {{ nrWeeksToShow }} weeks:</h3>
+
+    <calendar :nrWeeksToShow="nrWeeksToShow"/>
+  </div>
 </template>
 
 <script>
+import WeekSchedule from "@/components/WeekSchedule.vue";
 import Calendar from "@/components/Calendar.vue";
+
+import classesJson from '@/data/classes.json';
 
 export default {
   name: 'ClassSchedule',
   components: {
+    WeekSchedule,
     Calendar,
   },
   props: {
@@ -17,9 +29,21 @@ export default {
   },
   data() {
     return {
+      classes: classesJson.classes,
+      categories: ["music", "theater", "class", "arts"]
     }
   },
   mounted () {
+     /* class: {
+     *   title: "",
+     *   presenter: "",
+     *   price: "",
+     *   description: "",
+     *   date: { start: "", end: "" },
+     *   time: { start: "", end: "" },
+     *   cat: "",
+     * }
+     */
   },
   methods: {
   }
