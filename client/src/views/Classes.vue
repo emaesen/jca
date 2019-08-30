c1bc8406d371d8aa492c29e2da45f08f1f23b2dd
Edward <edward.maesen@gmail.com>
2019-08-30 13:18:16 -0700

    Add non-weekly class events

--- client/src/views/Classes.vue
+++ client/src/views/Classes.vue
@@ -2,18 +2,21 @@
   <div>
     <h1>Classes at JCA</h1>
 
-    <class-schedule :nrWeeksToShow="nrWeeksToShow"/>
+    <event-list type="class"/>
 
+    <class-schedule :nrWeeksToShow="nrWeeksToShow"/>
   </div>
 </template>
 
 <script>
 import ClassSchedule from "@/components/ClassSchedule.vue";
+import EventList from "@/components/EventList.vue";
 
 export default {
   name: 'Classes',
   components: {
     ClassSchedule,
+    EventList,
   },
   data() {
     return {
