0d9f69cfa2d288034bf019db527c3d9805236c67
Edward <edward.maesen@gmail.com>
2019-09-04 14:09:18 -0700

    Add submission section

--- client/src/views/VenueInfoBooking.vue
+++ client/src/views/VenueInfoBooking.vue
@@ -12,12 +12,36 @@
       </a>
     </p>
 
+    <h2>Musicians, Visual Artists, Performers:</h2>
+    <p class="emph">
+      We welcome your submissions year round.
+    </p>
+    <p>
+      Please send demos, bios, photos and anything else that showcases you to <a href="mailto:TheJCA@yahoo.com">TheJCA@yahoo.com</a> - or simply use the form below to start an email message.<br>
+    </p>
+
+    <contact-us-section/>
+
+    <p>
+      Alternatively, you may contact us directly at <a href="tel:+15309188383">(530) 918-8383</a> or send your submission by postal mail:
+      <pre>
+   Jefferson Center for the Arts
+   1124 Pine Grove Dr
+   Mt Shasta, CA 96067
+      </pre>
+
+    </p>
   </div>
 </template>
 
 <script>
+import ContactUsSection from '@/components/ContactUsSection.vue';
+
 export default {
   name: 'VenueInfoBooking',
+  components: {
+    ContactUsSection,
+  },
   data() {
     return {
     }
