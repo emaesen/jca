ec8dbe4703ceeaf78dd0ac868a7dc90ac5d02a58
Edward <edward.maesen@gmail.com>
2019-09-04 12:24:45 -0700

    Add location map

--- client/src/views/VenueInfoBooking.vue
+++ client/src/views/VenueInfoBooking.vue
@@ -2,9 +2,12 @@
   <div>
     <h1>Venue Info &amp; Booking</h1>
 
-    <h2></h2>
+    <h2>Location</h2>
 
     <p>
+      Jefferson Center for the Arts is located at just a few minutes drive from Mount Shasta city center:<br/>
+      
+      <img class="streetmap" src="/img/capture-openstreetmap-jca.png" alt="street map Mount Shasta and JCA"/>
     </p>
 
   </div>
@@ -22,5 +25,9 @@ export default {
 
 <style lang="less" scoped>
 @import '../assets/variab.less';
-  
+
+.streetmap {
+  margin: 2em auto;
+  width: 100%;
+}
 </style>
