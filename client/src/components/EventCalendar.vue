8a11185d476f14f386569a66250a4bdca7251eb1
Edward <edward.maesen@gmail.com>
2019-09-11 15:33:53 -0700

    Switch to event mixin

--- client/src/components/EventCalendar.vue
+++ client/src/components/EventCalendar.vue
@@ -19,24 +19,12 @@
 import Calendar from "./Calendar";
 
 import eventsJson from '@/data/events.json';
-//import classesJson from '@/data/classes.json';
 
-import date from './mixins/date.js'
-
-function uniqueId() {
-  return Math.random().toString(36).substr(2, 8) 
-    + Math.random().toString(36).substr(2, 8);
-}
-function mergeArrays(arr1,arr2) {
-  let arr = [];
-  Array.prototype.push.apply(arr, arr1);
-  Array.prototype.push.apply(arr, arr2);
-  return arr;
-}
+import event from './mixins/event.js';
 
 export default {
   name: "EventCalendar",
-  mixins: [date],
+  mixins: [event],
   components: {
     Calendar,
   },
@@ -50,69 +38,15 @@ export default {
     };
   },
   mounted() {
-    this.setEventCategories();
-    this.setEventTypes();
-  },
-  created() {
-    
+    this.eventCategories = this.evt__eventCategories(this.events);
+    this.eventTypes = this.evt__eventTypes(this.events);
   },
   computed: {
     events() {
-      // let allEvents = mergeArrays(eventsJson.events, this.recurringEvents);
-      let allEvents = eventsJson.events;
-      let preparedEvents = allEvents
-        .filter(e => !this.isPastDate(
-          e.date && e.date.start ? (e.date.end ? e.date.end : e.date.start) : "2052-01-01") )
-        .map(e => {
-          if(e.date) {
-            if(!e.date.end && !e.weekdays) {
-              e.date.end = e.date.start
-            } 
-          }
-          return e;})
-        .map(e => {e._id = uniqueId(); return e});
-
-      return preparedEvents;
-    },
-    recurringEvents() {
-      let events = [];
-      let weeklyClasses = classesJson.classes.weekly;
-      let weekdays = this.dateNames.days.map(d => d.toLowerCase());
-
-      weekdays.forEach((wd,i) => {
-        if (weeklyClasses[wd] && weeklyClasses[wd].length > 0) {
-          weeklyClasses[wd].forEach(e => {
-            e.weekdays=[i]; 
-            e.type="class"; 
-            if (!e.date) {
-              e.date = {};
-            }
-            events.push(e);
-          })
-        }
-      })
-
-      return events;
-    },
-    noWeeklyEventDates() {
-      return classesJson.classes.noClassesDates;
+      return this.evt__filteredEvents(eventsJson.events);
     },
   },
   methods: {
-    setEventCategories() {
-      // get list of eventCategories and remove duplicates
-      this.eventCategories = this.events
-        .map(e => e.category)
-        .filter((c, i, s) => s.indexOf(c) === i)
-        .sort();
-    },
-    setEventTypes() {
-      // get list of eventTypes and remove duplicates
-      this.eventTypes = this.events
-        .map(e => e.type)
-        .filter((c, i, s) => s.indexOf(c) === i)
-        .sort();
-    },
   }
 };
 </script>
