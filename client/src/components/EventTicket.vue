ce7f9173933824129a3bd18acaf5c0c4e66cf40a
Edward <edward.maesen@gmail.com>
2019-09-12 17:44:55 -0700

    Add modal with ticket purchase iframe

--- client/src/components/EventTicket.vue
+++ client/src/components/EventTicket.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
-    <a :href="href" 
-      title="click to buy ticket" 
-      target="_blank" rel="noopener noreferrer"
-    >
+    
+    <span @click="openModal">
       <icon-ticket class="action"/>
-    </a>
+    </span>
+
+    <modal
+      v-if="showModal"
+      @close="closeModal"
+    >
+      <iframe class="iframe-class" width="100%" height="600px" src="https://jeffersoncenterforthearts.thundertix.com/events/158781" frameborder="0" scrolling="auto" marginheight="0" marginwidth="0" allowtransparency="true" ></iframe>
+    </modal>
   </div>
 </template>
 
 <script>
 import IconTicket from '@/components/icons/IconTicket.vue';
+import Modal from '@/components/Modal.vue';
 
 export default {
   name: 'EventTicket',
   components: {
     IconTicket,
+    Modal,
   },
   props: {
     id: {
@@ -24,6 +31,7 @@ export default {
   },
   data() {
     return {
+      showModal: false,
       baseUrl: "https://jeffersoncenterforthearts.thundertix.com/events/",
     }
   },
@@ -31,6 +39,14 @@ export default {
     href() {
       return this.baseUrl + this.id;
     }
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
   }
 };
 </script>
