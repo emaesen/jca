5f7b9cc1a3e81374803d9c817ee34e56a39a5119
Edward <edward.maesen@gmail.com>
2019-09-14 09:19:23 -0700

    Add volunteers for events section

--- client/src/views/GetInvolved.vue
+++ client/src/views/GetInvolved.vue
@@ -6,10 +6,16 @@
 
     <h2>Volunteers:</h2>
     <p>
-      <span class="emph">Please <link-outbound to="https://www.facebook.com/wendybirdfly">contact Wendy on Facebook</link-outbound> if you are interested in volunteering at the JCA.</span>
+      <span class="emph">Please <router-link to="contact">contact Wendy</router-link> if you are interested in volunteering at the JCA.</span>
       We can always use more help with fund-raising, grant writing, painting, design and the like.<br class="spacer">
       It takes a village…<br class="spacer">
       Lets make some MAGIC!
+    </p>
+    <h4>…Volunteers for events</h4>
+    <p>
+      We also ask for volunteers for each event, folks to run the door, concessions etc..<br class="spacer">
+      Volunteers get free admission to the event, food, drink and lots of love!<br class="spacer">
+      Click on the "volunteer" button in any of the <router-link to="events">event listings</router-link> to send an email to Wendy if you are interested in volunteering for that particlar event.
     </p>
 
 
