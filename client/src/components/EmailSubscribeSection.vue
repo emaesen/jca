238d594bff69b6aff04344f1982f6b03ddb4d1a6
Edward <edward.maesen@gmail.com>
2019-09-18 08:52:29 -0700

    Prevent eslint warnings

--- client/src/components/EmailSubscribeSection.vue
+++ client/src/components/EmailSubscribeSection.vue
@@ -161,6 +161,7 @@ export default {
   },
   computed: {
     emailFormatIsValid() {
+      // eslint-disable-next-line no-control-regex
       const re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
       return re.test(this.email);
     },
@@ -175,7 +176,7 @@ export default {
     },
   },
   methods: {
-    submitForm(event) {
+    submitForm() {
       this.isFormSubmitted = true;
       this.showModal = true;
     },
