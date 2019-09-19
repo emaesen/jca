4611379818ca223d0f49df66ffc177c955f6fed8
Edward <edward.maesen@gmail.com>
2019-09-19 11:29:37 -0700

    Add explanation for page not found

--- client/src/views/PageNotFound.vue
+++ client/src/views/PageNotFound.vue
@@ -4,18 +4,25 @@
     <p> - - - - - </p>
     <p class="anima__fade-in-bottom delay1">S O</p>
     <p class="anima__fade-in-bottom delay2">S O R R Y</p>
+    <p class="anima__fade-in-bottom delay3 spacious">
+      The page you are looking for does not –or no longer– exist.<br>
+      Please click any of the links in the navigation bar above to continue.<br>
+    </p>
   </div>
 </template>
 
 
 <style lang="less" scoped>
 .pnf {
-  margin: 12em 0 30em 0;
+  margin: 8em 0 30em 0;
 }
 .delay1 {
   animation-delay: 1s;
 }
 .delay2 {
   animation-delay: 1.1s;
+}
+.delay3 {
+  animation-delay: 2s;
 }
 </style>
