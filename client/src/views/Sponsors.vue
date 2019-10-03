2dccd89dead63d439068a5077099983d44dc118f
Edward <edward.maesen@gmail.com>
2019-10-02 17:03:46 -0700

    Add sponsors view

---
+++ client/src/views/Sponsors.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>JCA Sponsors</h1>
+
+    <sponsor 
+      v-for="(sponsor, index) in sponsors" 
+      :key="sponsor.name" 
+      :sponsor="sponsor"
+      class="anima__zoom"
+      :style="'animation-delay:' + index/5 + 's;animation-fill-mode: backwards;'"
+    />
+
+  </div>
+</template>
+
+<script>
+import Sponsor from '@/components/Sponsor.vue';
+
+import sponsorsJson from '@/data/sponsors.json';
+
+export default {
+  name: 'Sponsors',
+  components: {
+    Sponsor,
+  },
+  data() {
+    return {
+      sponsors: sponsorsJson.sponsors,
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
