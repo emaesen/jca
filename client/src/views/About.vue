11d01ae1f7ef998d825999e6caf11924535f1eea
Edward <edward.maesen@gmail.com>
2019-10-07 08:53:37 -0700

    Add zoom animation

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -8,7 +8,7 @@
       Jefferson Center for the Arts (JCA) is Mount Shasta’s premier arts education venue incorporating the natural beauty of the area for the purpose of improving social, economic and cultural assets. The JCA is a regional hub for Live Music, Theatre and Visual Arts for local residents and tourists alike.
     </p>
 
-    <img class="img-hero" src="/img/jca-banner.png" alt="JCA banner"/>
+    <img class="img-hero anima__zoom" src="/img/jca-banner.png" alt="JCA banner"/>
 
     <!--<privacy-statement/>-->
   </div>
